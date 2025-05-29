00b72bc2bae5506b2ac6da9f50d0b5c0ea2d09f0
gabrielHernandez2019 <gabriel.hernandez.u@gmail.com>
2025-05-28 22:30:26 -0400

    Update ProductControllerTest.java
    se eliminan test que aportan valor

--- src/test/java/com/zara/price/infrastructure/adapter/rest/ProductControllerTest.java
+++ src/test/java/com/zara/price/infrastructure/adapter/rest/ProductControllerTest.java
@@ -18,23 +18,6 @@ import static org.mockito.Mockito.*;
 
 class ProductControllerTest {
 
-    @Test
-    void testRequestAt10AMOn14th() {
-        ProductService mockService = mock(ProductService.class);
-        ProductController controller = new ProductController(mockService);
-
-        LocalDateTime applicationDate = LocalDateTime.of(2023, 6, 14, 10, 0);
-        Integer productId = 35455;
-        Integer brandId = 1;
-
-        Product mockProducts = new Product(1,brandId, applicationDate, LocalDateTime.of(2023, 6, 14, 23, 59), BigDecimal.valueOf(35.50));
-        when(mockService.findProductsByCriteria(applicationDate, productId, brandId)).thenReturn(mockProducts);
-
-        Product result = controller.getPriceForProduct(applicationDate, productId, brandId);
-
-        assertEquals(mockProducts, result);
-        verify(mockService).findProductsByCriteria(applicationDate, productId, brandId);
-    }
 
     @Test
     void testRequestAt4PMOn14th() {
@@ -72,59 +55,7 @@ class ProductControllerTest {
         verify(mockService).findProductsByCriteria(applicationDate, productId, brandId);
     }
 
-    @Test
-    void testRequestAt10AMOn15th() {
-        ProductService mockService = mock(ProductService.class);
-        ProductController controller = new ProductController(mockService);
 
-        LocalDateTime applicationDate = LocalDateTime.of(2023, 6, 15, 10, 0);
-        Integer productId = 35455;
-        Integer brandId = 1;
-
-        Product mockProducts = new Product();
-        when(mockService.findProductsByCriteria(applicationDate, productId, brandId)).thenReturn(mockProducts);
-
-        Product result = controller.getPriceForProduct(applicationDate, productId, brandId);
-
-        assertEquals(mockProducts, result);
-        verify(mockService).findProductsByCriteria(applicationDate, productId, brandId);
-    }
-
-    @Test
-    void testRequestAt9PMOn16th() {
-        ProductService mockService = mock(ProductService.class);
-        ProductController controller = new ProductController(mockService);
-
-        LocalDateTime applicationDate = LocalDateTime.of(2023, 6, 16, 21, 0);
-        Integer productId = 35455;
-        Integer brandId = 1;
-
-        Product mockProducts = new Product();
-        when(mockService.findProductsByCriteria(applicationDate, productId, brandId)).thenReturn(mockProducts);
-
-        Product result = controller.getPriceForProduct(applicationDate, productId, brandId);
-
-        assertEquals(mockProducts, result);
-        verify(mockService).findProductsByCriteria(applicationDate, productId, brandId);
-    }
-
-    @Test
-    void testGetPriceForProduct() {
-        ProductService mockService = mock(ProductService.class);
-        ProductController controller = new ProductController(mockService);
-
-        LocalDateTime applicationDate = LocalDateTime.now();
-        Integer productId = 1;
-        Integer brandId = 1;
-
-        Product mockProducts = new Product();
-        when(mockService.findProductsByCriteria(applicationDate, productId, brandId)).thenReturn(mockProducts);
-
-        Product result = controller.getPriceForProduct(applicationDate, productId, brandId);
-
-        assertEquals(mockProducts, result);
-        verify(mockService).findProductsByCriteria(applicationDate, productId, brandId);
-    }
 
     @Test
     void testGetPriceForProductWithEmptyResult() {
